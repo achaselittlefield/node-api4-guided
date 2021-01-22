dab7faf4aa720a77c612cbf07b04f39a5904528a
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2021-01-21 19:18:35 -0800

    Finished Project

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const motd = process.envMOTD || "hello world"
+  res.status(200).json({ api: "up", motd: process.env.MOTD });
 });
 
 router.get("/shouts", (req, res, next) => {
